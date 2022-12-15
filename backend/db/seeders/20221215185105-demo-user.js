18db33f589526bacf5829b1fe9b2a14d3a85d46a
Le Nguyen <ltnguyen517@gmail.com>
2022-12-15 18:40:19 -0500

    completed validating the request body

--- backend/db/seeders/20221215185105-demo-user.js
+++ backend/db/seeders/20221215185105-demo-user.js
@@ -11,16 +11,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        firstName: 'Lebron',
+        lastName: 'James',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Olivia',
+        lastName: 'Bir',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Bob',
+        lastName: 'Builder',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
