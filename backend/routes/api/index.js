52649b793311e4a107041225ac21ae09c9cc25eb
Le Nguyen <ltnguyen517@gmail.com>
2022-12-22 15:28:30 -0500

    completed get all reviews of current user

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 // const spotImagesRouter = require('./spot-images.js');
-// const reviewsRouter = require('./reviews.js');
+const reviewsRouter = require('./reviews.js');
 // const reviewImagesRouter = require('./review-images.js');
 // const bookingsRouter = require('./bookings.js');
 const { restoreUser } = require('../../utils/auth.js');
@@ -13,7 +13,7 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 // router.use('/spot-images', spotImagesRouter);
-// router.use('/reviews', reviewsRouter);
+router.use('/reviews', reviewsRouter);
 // router.use('/review-images', reviewImagesRouter);
 // router.use('/bookings', bookingsRouter);
 
---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { User, Spot, SpotImage, Review, ReviewImage, Booking } = require('../../db/models');
+const { check } = require("express-validator");
+const { handleValidationErrors } = require('../../utils/validation');
+const sequelize = require("sequelize");
+const { validateLogin } = require('./session');
+
+//Get all reviews of the current user
+router.get('/current', requireAuth, async (req, res) => {
+    const reviews = await Review.findAll({
+        where: {
+            userId: req.user.id
+        },
+        attributes: [
+            'id',
+            'userId',
+            'spotId',
+            'review',
+            'stars',
+            'createdAt',
+            'updatedAt'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'firstName', 'lastName']
+            },
+            {
+                model: Spot,
+                attributes: {
+                    exclude: ['description', 'createdAt', 'updatedAt']
+                }
+            },
+            {
+                model: ReviewImage,
+                attributes: ['id', 'url']
+            }
+        ]
+    });
+    for(let i = 0; i < reviews.length; i++){
+        reviews[i] = reviews[i].toJSON();
+        let previewImage = await SpotImage.findOne({
+            where: {
+                spotId: reviews[i].spotId,
+                preview: true
+            }
+        })
+        if(previewImage){
+            reviews[i].Spot.previewImage = previewImage.url
+        } else {
+            reviews[i].Spot.previewImage = null
+        }
+    }
+    return res.json({
+        "Reviews": reviews
+    })
+});
+
+
+
+
+
+
+module.exports = router;
