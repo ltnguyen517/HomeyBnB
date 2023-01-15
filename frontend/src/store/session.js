98a5209d555d566aa25b672ffb8b3a5e30734621
Le Nguyen <ltnguyen517@gmail.com>
2023-01-15 14:02:29 -0500

    added a logout thunk action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -52,7 +52,16 @@ export const signup = (user) => async (dispatch) => {
     const data = await response.json();
     dispatch(setUser(data.user));
     return response;
+};
+
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
   };
+
 
 
 
