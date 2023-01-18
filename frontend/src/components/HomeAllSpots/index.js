fbba816ceb2997fc63697b1e4e322bb86fb588ae
Le Nguyen <ltnguyen517@gmail.com>
2023-01-17 20:20:22 -0500

    Working on layout of the home page

--- frontend/src/components/HomeAllSpots/index.js
+++ frontend/src/components/HomeAllSpots/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";;
 import { getAllSpots } from "../../store/spots";
 import { Link } from "react-router-dom";
 import "./HomeSpots.css";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";;
+
 
 const HomeSpotsPage = () => {
     const dispatch = useDispatch();
@@ -17,10 +18,29 @@ const HomeSpotsPage = () => {
         <div className="mainspot-container">
             {spots.map((spot) => (
                 <div className="spot-container" key={spot.id}>
-                    <SpotCard spot={spot} />
+                    <div className="placeslayout">
+                        <div className="eachspot-container">
+                            <Link className="place-img" to={`/spots/${spot.id}`}>
+                                <img className="spot-img" src={spot.previewImage} alt="Home" />
+                                <div className="vertexofcard-container">
+                                    <div className="city-info">{spot.city + ", " + spot.state}</div>
+                                    <div className="info">
+                                        <div className="spot-stars">
+                                            <i className="fa-solid fa-star"></i>
+                                            {spot.avgRating}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="bottomofcard">
+                                    <div className="card-price">{"$" + spot.price}</div>
+                                    <div className="night-word">{"night"}</div>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
     );
 };
-export default HomeSpotsPage
+export default HomeSpotsPage;
