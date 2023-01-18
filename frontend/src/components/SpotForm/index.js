34fdca8dc38a369333c72745bd377bd10d7616b6
Le Nguyen <ltnguyen517@gmail.com>
2023-01-18 15:04:27 -0500

    Spot form completed

--- frontend/src/components/SpotForm/index.js
+++ frontend/src/components/SpotForm/index.js
@@ -16,51 +16,143 @@ const SpotForm = () => {
     const [ previewImage, setPreviewImage ] = useState("");
     const [ validationErrors, setValidationErrors ] = useState([]);
     const [ uponCompletion, setUponCompletion ] = useState(false);
-}
-if(uponCompletion) return <Redirect to="/" />;
 
-const currentUser = useSelector(state => state.session.user);
+    if(uponCompletion) return <Redirect to="/" />;
 
-useEffect(() => {
-    const errors = [];
+    const currentUser = useSelector(state => state.session.user);
 
-    if(!currentUser) errors.push("Must be logged in for you to create a spot");
-    if(address.length === 0) errors.push("Please enter a valid street address");
-    if(city.length === 0) errors.push("Please enter a valid city");
-    if(state.length === 0) errors.push("Please enter a valid state");
-    if(country.length === 0) errors.push("Please enter a valid country");
-    if(name.length === 0) errors.push("Please enter a name. It is required");
-    if(description.length === 0) errors.push("Please enter a valid description. It is required");
-    if(price < 0) errors.push("Please enter a price greater than or equal to 0");
-    if(!previewImage) errors.push("Please enter a valid image URL");
+    useEffect(() => {
+        const errors = [];
 
-    setValidationErrors(errors);
-}, [currentUser, address, city, state, country, name, description, price, previewImage]);
+        if(!currentUser) errors.push("Must be logged in for you to create a spot");
+        if(address.length === 0) errors.push("Please enter a valid street address");
+        if(city.length === 0) errors.push("Please enter a valid city");
+        if(state.length === 0) errors.push("Please enter a valid state");
+        if(country.length === 0) errors.push("Please enter a valid country");
+        if(name.length === 0) errors.push("Please enter a name. It is required");
+        if(description.length === 0) errors.push("Please enter a valid description. It is required");
+        if(price < 0) errors.push("Please enter a price greater than or equal to 0");
+        if(!previewImage) errors.push("Please enter a valid image URL");
 
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    setValidationErrors([]);
+        setValidationErrors(errors);
+    }, [currentUser, address, city, state, country, name, description, price, previewImage]);
 
-    const brandNewSpot = {
-        address,
-        city,
-        state,
-        country,
-        name,
-        description,
-        price,
-        lat: 120.25,
-        lng: 75.89
-    }
-    const brandNewSpotImage = {
-        url: previewImage,
-        preview: true
-    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setValidationErrors([]);
 
-    return dispatch(createNovelSpot(brandNewSpot, brandNewSpotImage))
-        .then(async(res) => {setUponCompletion(true)})
-        .catch(async(res) => {
-            const newSpot = await res.json();
-            if(newSpot && newSpot.errors) setValidationErrors(newSpot.errors)
-        })
-}
+        const brandNewSpot = {
+            address,
+            city,
+            state,
+            country,
+            name,
+            description,
+            price,
+            lat: 120.25,
+            lng: 75.89
+        }
+        const brandNewSpotImage = {
+            url: previewImage,
+            preview: true
+        }
+
+        return dispatch(createNovelSpot(brandNewSpot, brandNewSpotImage))
+            .then(async(res) => {setUponCompletion(true)})
+            .catch(async(res) => {
+                const newSpot = await res.json();
+                if(newSpot && newSpot.errors) setValidationErrors(newSpot.errors)
+            })
+    };
+
+    return (
+        <>
+         <div className="complete-layout">
+            <div className="spotcreation-form">
+                <h1 className="spotform-header">Let's connect your home to HomeyBnb!</h1>
+                <form className="spot-form" onSubmit={handleSubmit}>
+                    <ul className="spot-form-errors">
+                        {validationErrors.map((error) => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                    <label>
+                        <input
+                            type="text"
+                            value={address}
+                            placeholder= 'Address'
+                            onChange={(e) => setAddress(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="text"
+                            value={city}
+                            placeholder= 'City'
+                            onChange={(e) => setCity(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="text"
+                            value={state}
+                            placeholder= 'State'
+                            onChange={(e) => setState(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="text"
+                            value={country}
+                            placeholder= 'Country'
+                            onChange={(e) => setCountry(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="text"
+                            value={name}
+                            placeholder= 'Name'
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="text"
+                            value={description}
+                            placeholder= 'Description'
+                            onChange={(e) => setDescription(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="number"
+                            value={price}
+                            placeholder= 'Price'
+                            onChange={(e) => setPrice(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="text"
+                            value={previewImage}
+                            placeholder= 'Img url'
+                            onChange={(e) => setPreviewImage(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <button className="spot-creation-button" type="submit">Create New Spot</button>
+                </form>
+            </div>
+         </div>
+        </>
+    );
+};
+export default SpotForm;
