aca64eedce39840fc7b149229bdd7a1c11c9293e
Le Nguyen <ltnguyen517@gmail.com>
2023-04-18 18:47:48 -0400

    added more bookings seeders

--- backend/db/seeders/20221220055402-Bookings.js
+++ backend/db/seeders/20221220055402-Bookings.js
@@ -11,45 +11,42 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        userId: 1,
+        userId: 3,
         startDate: '2023-01-10',
         endDate: '2023-01-15'
       },
       {
-        spotId: 2,
+        spotId: 3,
         userId: 2,
-        startDate: '2023-02-3',
+        startDate: '2023-02-03',
         endDate: '2023-02-15'
       },
       {
-        spotId: 3,
-        userId: 3,
+        spotId: 5,
+        userId: 1,
         startDate: '2023-05-15',
         endDate: '2023-05-19'
+      },
+      {
+        spotId: 6,
+        userId: 4,
+        startDate: '2023-07-11',
+        endDate: '2023-07-14'
+      },
+      {
+        spotId: 18,
+        userId: 3,
+        startDate: '2023-09-20',
+        endDate: '2023-09-23'
       }
     ], {});
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
   },
 
   async down (queryInterface, Sequelize) {
     options.tableName = 'Bookings';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2, 3] }
+      spotId: { [Op.in]: [1, 3, 5, 6, 18] }
     }, {});
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
   }
 };
