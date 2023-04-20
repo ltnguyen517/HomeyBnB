7ca3f85fb238744fb8a1a55a0007b95c6aece631
Le Nguyen <ltnguyen517@gmail.com>
2023-04-20 17:01:07 -0400

    trying to fix render issue

--- backend/db/migrations/20221215183607-create-user.js
+++ backend/db/migrations/20221215183607-create-user.js
@@ -28,7 +28,7 @@ module.exports = {
         unique: true
       },
       email: {
-        type: Sequelize.STRING(255),
+        type: Sequelize.STRING(256),
         allowNull: false,
         unique: true
       },
